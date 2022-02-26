beaac349c4984916bc2819745a35b1196d90a69a
Cristian Perdomo <ferperdomo14@gmail.com>
2022-02-26 10:29:15 +0100

    navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,42 +4,46 @@ const Navigation = () => {
 
     return (
         <Navbar className='navigator' expand={false}>
-            <Container fluid>
-                <img src='https://asobal.es/img/logo.png' />
+            <Container fluid >
 
-                <Button className='navButton' size="sm">Registro</Button>
-                <Button className='navButton' size="sm">Iniciar sesión</Button>
+                {/* <Button className='navButton' size="sm">Registro</Button>
+                <Button className='navButton' size="sm">Iniciar sesión</Button> */}
                 <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                <img src='https://asobal.es/img/logo.png' />
+                <img className='navImage' src='https://img.icons8.com/material-outlined/344/search--v1.png' />
                 <Navbar.Offcanvas
                     id="offcanvasNavbar"
                     aria-labelledby="offcanvasNavbarLabel"
-                    placement="end"
+                    placement="start"
+                    className='offCanvas text-align-left'
                 >
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel">Asobal</Offcanvas.Title>
+                    <Offcanvas.Header closeButton className='offCanvas'>
+                        <Offcanvas.Title id="offcanvasNavbarLabel" ></Offcanvas.Title>
                     </Offcanvas.Header>
-                    <Offcanvas.Body className='text-end justify-content-end'>
+                    <Offcanvas.Body className='offCanvas justify-content-end'>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Navbar.Collapse className='text-end justify-content-end' id="basic-navbar-nav">
+                            <Navbar.Collapse className='text-start justify-content-start' id="basic-navbar-nav">
                                 <Nav >
-                                    <Nav.Link href="#action/3.1">Tickets</Nav.Link>
-                                    <Nav.Link href="#action/3.2">Estadísticas</Nav.Link>
-                                    <Nav.Link href="#action/3.3">Clubs</Nav.Link>
-                                    <Nav.Link href="#action/3.3">Prensa</Nav.Link>
+                                    <Nav.Link href="#action/3.1">Perfil</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.2">Noticias</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.3">Competiciones</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.3">Clubes</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.4">Prensa</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.4">Tickets</Nav.Link>
+                                    <hr />
+                                    <Nav.Link href="#action/3.4">Tienda</Nav.Link>
+                                    <hr />
                                     <Nav.Link href="#action/3.4">Sorteos</Nav.Link>
+                                    <hr />
                                 </Nav>
                             </Navbar.Collapse>
                         </Nav>
-                        <Form className="d-flex">
-                            <FormControl
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <br />
-                            <Button variant="dark">Search</Button>
-                        </Form>
+
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Container>
